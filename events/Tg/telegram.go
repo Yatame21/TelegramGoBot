6035cdafa95beea9eec3d2aa6455de5425c38f8d
yatame <maximtoshov21@gmail.com>
2026-07-24 23:26:10 +0300

    I am starting to write EventProcessor

--- events/Tg/telegram.go
+++ events/Tg/telegram.go
@@ -3,9 +3,24 @@ package Tg
 import "TestGOBot/clients/telegram"
 
 type Dispatcher struct {
-	tg     *telegram.Client
-	offset int
-	// storage
+	tg      *telegram.Client
+	offset  int
+	storage storage.Storage
 }
 
-// func New sdfsdf
+func New(client *telegram.Client, storage storage.Storage) *Processor {
+	return &Processor{
+		tg:      client,
+		storage: storage,
+	}
+}
+
+func (p *Processor) Fetch(limit int) ([]events.Event, error) {
+	update, err := p.tg.Updates(p.offset, limit)
+	if err != nil {
+		return nil, e.Wrap("cannot get events", err)
+	}
+
+	res := make([]evets.Event, 0, len(update))
+
+}
