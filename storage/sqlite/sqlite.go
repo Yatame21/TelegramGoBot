7ee6d3c50ff853b4482cb9dea77435347773cf90
yatame <maximtoshov21@gmail.com>
2026-08-27 14:58:39 +0300

    Save() & PickRandom in sql

--- storage/sqlite/sqlite.go
+++ storage/sqlite/sqlite.go
@@ -8,4 +8,40 @@ type Storage struct {
 
 func New(path string) (*Storage, error) {
 	db, err := sql.Open("sqlite3", path)
+	if err != nil {
+		return nil, fmt.Errorf("cannot open database: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("cannot connect to database: %w", err)
+	}
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) Save(ctx context.Context, p *storage.Page) error {
+	q := `INSERT INTO pages (url, user_name) VALUES (?, ?)`
+
+	if _, err := s.db.ExecContext(ctx, q, p.URL, p.UserName); err != nil {
+		return fmt.Errorf("cannot save page: %w", err)
+	}
+	return nil
+}
+
+func (s *Storage) PickRandom(ctx context.Context, userName string) (*storage.Page, error) {
+	q := `SELECT url FROM pages WHERE user_name = ? ORDER BY RANDOM() LIMIT 1`
+
+	var url string
+
+	err := s.db.QueryRowContext(ctx, q, userName).Scan(&url)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, fmt.Errorf("cannot pick random page: %w", err)
+	}
+
+	return &storage.Page{
+		URL:      url,
+		userName: userName,
+	}, nil
 }
